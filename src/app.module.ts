88b31abd48be36e31a5626b630b2135e979d5149
Twin <scientists.twin1@gmail.com>
2023-12-12 18:58:46 +0400

    feat: a telegram bot was init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import baseConfig from './config/base.config';
 import neo4jConfig from './config/neo4j.config';
 import telegramConfig from './config/telegram.config';
 import validationSchema from './config/configs.schema';
+import { NestjsGrammyModule } from '@grammyjs/nestjs';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       validationSchema: validationSchema,
       isGlobal: true,
       load: [baseConfig, neo4jConfig, telegramConfig],
+    }),
+    NestjsGrammyModule.forRootAsync({
+      botName: 'TogetherCrew',
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        token: configService.get<string>('telegram.token'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
