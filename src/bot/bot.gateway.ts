877e92726d6115f7eda12d93875cab520ff1e7f0
Twin <scientists.twin1@gmail.com>
2024-01-17 16:46:25 +0400

    feat: tow decorator were added to managed the telegram responses

--- src/bot/bot.gateway.ts
+++ src/bot/bot.gateway.ts
@@ -1,6 +1,36 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { BotService } from './bot.service';
 
+const groupNewTextMessageMetaKeyPrefix = 'GroupNewTextMessage';
+const generateTextMessageMetaKey = (text?: string) => {
+  if (text) return `${groupNewTextMessageMetaKeyPrefix}-${text}`;
+  else return groupNewTextMessageMetaKeyPrefix;
+};
+function OnGroupNewTextMessage(text?: string) {
+  return function (_target: any, propertyKey: string) {
+    Reflect.defineMetadata(
+      generateTextMessageMetaKey(text),
+      propertyKey,
+      BotGateway,
+    );
+  };
+}
+
+const groupEditedTextMessageMetaKeyPrefix = 'GroupEditedTextMessage';
+const generateEditedTextMessageMetaKey = (text?: string) => {
+  if (text) return `${groupEditedTextMessageMetaKeyPrefix}-${text}`;
+  else return groupEditedTextMessageMetaKeyPrefix;
+};
+function OnGroupEditedTextMessage(text?: string) {
+  return function (_target: any, propertyKey: string) {
+    Reflect.defineMetadata(
+      generateEditedTextMessageMetaKey(text),
+      propertyKey,
+      BotGateway,
+    );
+  };
+}
+
 @Injectable()
 export class BotGateway {
   constructor(private botService: BotService) {}
