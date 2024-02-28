91a88f5bedc4b76f1a4f773927c25de2483dba6f
Cyrille Derche <c.derche@me.com>
2024-02-28 22:39:53 +0100

    rmq module spec

--- libs/common/src/rmq/rmq.module.spec.ts
+++ libs/common/src/rmq/rmq.module.spec.ts
@@ -2,9 +2,19 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ConfigModule } from '@nestjs/config';
 import { RmqService } from './rmq.service';
 import { RmqModule } from './rmq.module';
-import { rmqConfig } from '../config/rmq.config';
-import { schemaConfig } from '../config/schema.config';
-import { telegramConfig } from '../config/telegram.config';
+// import { rmqConfig } from '../config/rmq.config';
+// import { schemaConfig } from '../config/schema.config';
+// import { telegramConfig } from '../config/telegram.config';
+
+// const mockConfig = {
+//   RMQ_HOST: 'mock_rmq_host',
+//   RMQ_USER: 'mock_rmq_user',
+//   RMQ_PASS: 'mock_rmq_pass',
+//   TELEGRAM_TOKEN: 'mock_telegram_token',
+//   MONGO_HOST: 'mock_mongo_host',
+//   MONGO_USER: 'mock_mongo_user',
+//   MONGO_PASS: 'mock_mongo_pass',
+// };
 
 describe('RmqModule', () => {
   let service: RmqService;
@@ -14,8 +24,6 @@ describe('RmqModule', () => {
     module = await Test.createTestingModule({
       imports: [
         ConfigModule.forRoot({
-          validationSchema: schemaConfig,
-          load: [telegramConfig, rmqConfig],
           isGlobal: true,
         }),
         RmqModule.register({
